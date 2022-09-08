249a66e25e1c0e176d4d40b1c91863638e54033f
Linerosky <112550787+Linerosky@users.noreply.github.com>
2022-09-07 23:34:08 -0500

    Add files via upload

--- datadb.php
+++ datadb.php
@@ -1,19 +1,7 @@
 <?php
-$conn = mysqli_connect("127.0.0.1:3306", "root", "", "pdesign");  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM pdesign.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$conn = mysqli_connect("database-1.clynxv1ewc7s.us-east-1.rds.amazonaws.com:3306", "admin", "mainreyna", "gps");  // Establece conexi�n
+$rows = mysqli_query($conn, "SELECT * FROM gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$fila = mysqli_fetch_assoc($rows);
+echo json_encode($fila);
 ?>
-<table border = 1 cellpadding = 10>     <!-- Se genera una tabla (la cual se entregara a index.php) -->
-  <tr>
-    <td>Latitud</td>  <!-- Info de las columnas -->
-    <td>Longitud</td>
-    <td>Fecha</td>
-  </tr>
-  <?php $i = 1; ?>
-  <?php foreach($rows as $row) : ?>     <!-- Funci�n FOR para crear columnas el valor $row es el nombre de la columna en SQL -->
-    <tr>            
-      <td><?php echo $row["Latitud"]; ?></td>
-      <td><?php echo $row["Longitud"]; ?></td>
-      <td><?php echo $row["Fecha"]; ?></td>
-    </tr>
-  <?php endforeach; ?>
-</table>
+
