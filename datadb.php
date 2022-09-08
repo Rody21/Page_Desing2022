886c2cb5aba8c14e3a7528d7ced48b34f49d39c6
Linerosky <daniellinerohd@gmail.com>
2022-09-08 02:47:41 -0500

    test

--- datadb.php
+++ datadb.php
@@ -1,7 +1,6 @@
 <?php
-$conn = mysqli_connect("database-1.clynxv1ewc7s.us-east-1.rds.amazonaws.com:3306", "admin", "mainreyna", "gps");  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$conn = mysqli_connect("database-1.clynxv1ewc7s.us-east-1.rds.amazonaws.com:3306", "admin", "mainreyna", "gps");  // Establece conexión ## "link point", "user", "password", "database"
+$rows = mysqli_query($conn, "SELECT * FROM gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL ## Tener en cuenta: "gps.db" database.table
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
-
