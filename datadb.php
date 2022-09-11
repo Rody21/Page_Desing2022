2d64a28cb4e8a225d796eb566cf6d24e74034fbc
Diseng <mjguette21@gmail.com>
2022-09-10 22:17:28 -0500

    Actualizacion Marlon

--- datadb.php
+++ datadb.php
@@ -1,7 +1,6 @@
 <?php
-$conn = mysqli_connect("database-1.clynxv1ewc7s.us-east-1.rds.amazonaws.com:3306", "admin", "mainreyna", "gps");  // Establece conexi�n
+$conn = mysqli_connect("desing.cwpobsmp7w7k.us-east-1.rds.amazonaws.com", "admin", "Neverland21", "gps");  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
-
