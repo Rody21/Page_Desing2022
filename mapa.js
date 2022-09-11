d26e8fb4141224ae0c1e29ff06919ed69cf02828
Diseng <mjguette21@gmail.com>
2022-09-11 13:19:07 -0500

    Añado archivo yml

--- mapa.js
+++ mapa.js
@@ -1,41 +1,47 @@
+function todo() {
 
-
-function table(){
+  function table() {
     const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
-    xhttp.onload = function(){
-        var data = this.responseText
-        data = JSON.parse(data);
-        document.getElementById("lat").innerHTML = data.Latitud
-        document.getElementById("lng").innerHTML = data.Longitud
-        document.getElementById("date").innerHTML = data.Fecha
 
-        var container = L.DomUtil.get('map');
+    xhttp.onload = function () {
+      var data = this.responseText
+      data = JSON.parse(data);
+      document.getElementById("lat").innerHTML = data.Latitud
+      document.getElementById("lng").innerHTML = data.Longitud
+      document.getElementById("date").innerHTML = data.Fecha
 
-        if(container != null){
+      //Mapa
+      var container = L.DomUtil.get('map');
 
+      if (container != null) {
         container._leaflet_id = null;
+      }
 
-        }
-        
-        if(map) {
-        map.invalidateSize();
-        }
 
-        var map = L.map('map');
+      if (map) {
+        map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pueda cambiar actualmente la posición ##
+      }
 
-        map = L.map('map').setView([data.Latitud, data.Longitud], 17);
-        
-        var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://gpstrack.sytes.net/">GPS Tracker</a>'
-                }).addTo(map);
-        
-        var marker = L.marker([data.Latitud, data.Longitud]).addTo(map)
-                .bindPopup('<b>' + data.Fecha + '</b>' + '<br />' + data.Latitud + ' ' + data.Longitud).openPopup();
+      var map = L.map('map');
+
+      map = L.map('map').setView([data.Latitud, data.Longitud], 16);
+
+      var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://gpstracker.sytes.net/">GPS Tracker</a>'
+      }).addTo(map);
+
+      var marker = L.marker([data.Latitud, data.Longitud]).addTo(map)
+        .bindPopup('<b>' + data.Fecha + '</b>' + '<br />' + data.Latitud + ' ' + data.Longitud).openPopup();
     }
 
     xhttp.open("GET", "datadb.php");  // documento que estamos llamando
     xhttp.send();
   }
-  setInterval(function(){   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
-    table();
-  }, 5000);
+
+  //Funcion para el delay
+  setInterval(
+    function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
+      table();
+    }, 5000); // Cada 5 segundos
+}
