9e3828f10b9033a709f568c812d4a755001bed7c
Diseng <mjguette21@gmail.com>
2022-09-10 22:24:30 -0500

    Base de datos

--- datadb.php
+++ datadb.php
@@ -1,6 +1,6 @@
 <?php
-$conn = mysqli_connect("desing.cwpobsmp7w7k.us-east-1.rds.amazonaws.com", "admin", "Neverland21", "gps");  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$conn = mysqli_connect("desing.cwpobsmp7w7k.us-east-1.rds.amazonaws.com", "admin", "Neverland21", "Gps");  // Establece conexi�n
+$rows = mysqli_query($conn, "SELECT * FROM Gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
