16efe1b4969c301bd14cb39acad389b56b289e0b
Diseng <mjguette21@gmail.com>
2022-09-13 10:52:33 -0500

    Incluir variables de entorno

--- datadb.php
+++ datadb.php
@@ -1,6 +1,6 @@
 <?php
 include 'varia.php';
-$conn = mysqli_connect("desing.cwpobsmp7w7k.us-east-1.rds.amazonaws.com", $BUSER, "Neverland21", "Gps");  // Establece conexi�n
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM Gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
