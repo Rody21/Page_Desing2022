792a707e67c47f4c85417e1a7add1f4dc0d3992d
losadas <losadas@uninorte.edu.co>
2022-09-13 15:15:23 -0500

    actusant

--- datadb.php
+++ datadb.php
@@ -1,7 +1,7 @@
 <?php
 include 'varia.php';
 $conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $BName);  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM Gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+$rows = mysqli_query($conn, "SELECT * FROM Gps.tabledata ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
