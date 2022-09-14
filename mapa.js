a8d68899a746a787bd0d7505ecd0e6ff3f04d686
Diseng <mjguette21@gmail.com>
2022-09-13 22:17:08 -0500

    prueba new

--- mapa.js
+++ mapa.js
@@ -1,46 +1,51 @@
 function todo() {
 
-
-  const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
-  xhttp.onload = function () {
-    var data = this.responseText
-    data = JSON.parse(data);
-    document.getElementById("lat").innerHTML = data.Latitud
-    document.getElementById("lng").innerHTML = data.Longitud
-    document.getElementById("date").innerHTML = data.Fecha
-
-    //Mapa
-    var container = L.DomUtil.get('map')
-
-    //Mapas if
-    if (container != null) {
-      container._leaflet_id = null;
-    }
+  function table() {
 
 
-    if (map) {
-      map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pueda cambiar actualmente la posición ##
-    }
+    const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
+    xhttp.onload = function () {
+      var data = this.responseText
+      data = JSON.parse(data);
+      document.getElementById("lat").innerHTML = data.Latitud
+      document.getElementById("lng").innerHTML = data.Longitud
+      document.getElementById("date").innerHTML = data.Fecha
 
-    //Declaracion del mapa
-    var map = L.map('map');
+      //Mapa
+      var container = L.DomUtil.get('map')
 
-    map = L.map('map').setView([data.Latitud, data.Longitud], 13);
+      //Mapas if
+      if (container != null) {
+        container._leaflet_id = null;
+      }
 
-    //Estilo del mapa
-    var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-    function table() {
+      if (map) {
+        map.invalidateSize(); // Si hay un mapa, lo elimina para recrearlo y que se pueda cambiar actualmente la posición ##
+      }
+
+      //Declaracion del mapa
+      var map = L.map('map');
+
+      map = L.map('map').setView([data.Latitud, data.Longitud], 13);
+
+      //Estilo del mapa
+      var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
       //Marcador
       var marker = L.marker([data.Latitud, data.Longitud]).addTo(map)
         .bindPopup('<b>' + data.Fecha + '</b>' + '<br />' + data.Latitud + ' ' + data.Longitud).openPopup();
-
-      xhttp.open("GET", "datadb.php");  // documento que estamos llamando
-      xhttp.send();
     }
 
-    table()
-    setInterval(table, 3000)
+    xhttp.open("GET", "datadb.php");  // documento que estamos llamando
+    xhttp.send();
   }
+
+
+
+  //Funcion para el delay
+  setInterval(
+    function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
+      table();
+    }, 5000); // Cada 5 segundos
 }
