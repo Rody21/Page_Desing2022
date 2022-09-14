fd6b3d27584c9fdf5a4c1ce8071fec65a9fc9555
Diseng <mjguette21@gmail.com>
2022-09-13 22:29:40 -0500

    sin carga en index

--- mapa.js
+++ mapa.js
@@ -2,11 +2,12 @@ function todo() {
 
   function table() {
 
-
     const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
+
     xhttp.onload = function () {
       var data = this.responseText
       data = JSON.parse(data);
+      
       document.getElementById("lat").innerHTML = data.Latitud
       document.getElementById("lng").innerHTML = data.Longitud
       document.getElementById("date").innerHTML = data.Fecha
@@ -41,11 +42,10 @@ function todo() {
     xhttp.send();
   }
 
-
-
-  //Funcion para el delay
-  setInterval(
-    function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
-      table();
-    }, 5000); // Cada 5 segundos
 }
+
+//Funcion para el delay
+setInterval(
+  function () {   //creamos un intervalo para realizar el mismo request del archivo que genera la conexión con la base de datos
+    table();
+  }, 5000); // Cada 5 segundos
