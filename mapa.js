096a090585a547c331af83f2d6acea82101ce513
Diseng <mjguette21@gmail.com>
2022-09-13 21:56:12 -0500

    cambio js

--- mapa.js
+++ mapa.js
@@ -15,33 +15,36 @@ function todo() {
 
   //Declaracion del mapa
   var map = L.map('map');
-  map = L.map('map').setView([data.Latitud, data.Longitud], 16);
+
+  map = L.map('map').setView([data.Latitud, data.Longitud], 13);
+
+  //Estilo del mapa
+  var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 
   function table() {
     const xhttp = new XMLHttpRequest();  //creando el objeto para trabajar
 
-    xhttp.onload = function() {
+    xhttp.onload = function () {
       var data = this.responseText
       data = JSON.parse(data);
       document.getElementById("lat").innerHTML = data.Latitud
       document.getElementById("lng").innerHTML = data.Longitud
       document.getElementById("date").innerHTML = data.Fecha
 
-      
-      //Estilo del mapa
-      var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
       //Marcador
       var marker = L.marker([data.Latitud, data.Longitud]).addTo(map)
         .bindPopup('<b>' + data.Fecha + '</b>' + '<br />' + data.Latitud + ' ' + data.Longitud).openPopup();
+     
+        map.on('click',onMapClick);
     }
 
     xhttp.open("GET", "datadb.php");  // documento que estamos llamando
     xhttp.send();
   }
 
-  
+
 
   //Funcion para el delay
   setInterval(
