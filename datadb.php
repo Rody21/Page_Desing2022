ee29f79d7ae572ffe31128f9e8c3e946c93d5040
losadas <losadas@uninorte.edu.co>
2022-09-13 23:14:35 -0500

    funcionaconvariables

--- datadb.php
+++ datadb.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect("database-1.cpjk8vtdfv2l.us-east-1.rds.amazonaws.com", "losadas", "boysoldier28", "Gps");  // Establece conexi�n
+include "varia.php";
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $Bname);  // Establece conexi�n
 $rows = mysqli_query($conn, "SELECT * FROM Gps.tabledata ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
