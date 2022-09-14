55d29d4a9c1373bab740bbf64cc1c251dc621bee
fuentesrr <rrfuentes@uninorte.edu.co>
2022-09-13 23:08:08 -0500

    Variables de entorno

--- datadb.php
+++ datadb.php
@@ -1,6 +1,7 @@
 <?php
-$conn = mysqli_connect("database-1.clynxv1ewc7s.us-east-1.rds.amazonaws.com:3306", "admin", "mainreyna", "gps");  // Establece conexi�n
-$rows = mysqli_query($conn, "SELECT * FROM gps.db ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
+include "varia.php";
+$conn = mysqli_connect($Bendpoint, $BUSER, $Bclave, $Bname);  // Establece conexi n
+$rows = mysqli_query($conn, "SELECT * FROM bdesign.Basedatos ORDER BY Fecha DESC LIMIT 1"); // genera el query a SQL
 $fila = mysqli_fetch_assoc($rows);
 echo json_encode($fila);
 ?>
